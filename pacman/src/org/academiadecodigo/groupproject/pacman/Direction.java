ac5134923a257560641458c2cd8bcc494604e3e4
Code Cadet <codecadet@lovelace.local>
2019-02-26 18:38:21 -0100

    Direction Enum OppositeDirections / ChangeDirection to class ghost

--- pacman/src/org/academiadecodigo/groupproject/pacman/Direction.java
+++ pacman/src/org/academiadecodigo/groupproject/pacman/Direction.java
@@ -6,15 +6,71 @@ package org.academiadecodigo.groupproject.pacman;
 
 public enum Direction {
     UP,
-    Down,
-    RIGHT,
-    LEFT;
+    DOWN,
+    LEFT,
+    RIGHT;
 
-    public static Direction getRandom(){
+
+    public Direction getRandomDirection() {
+        //return Direction.values()[(int) (Math.random() * Direction.values().length)];
+
+
+        switch (this) {
+            case UP:
+                return changeFromVertical();
+
+            case DOWN:
+                return changeFromVertical();
+
+            case LEFT:
+                return changeFromHorizontal();
+
+            case RIGHT:
+                return changeFromHorizontal();
+        }
         return null;
     }
 
-    public static Direction change(){
+
+
+    public Direction getOpposite() {
+        switch (this) {
+            case UP:
+                return DOWN;
+
+            case DOWN:
+                return UP;
+
+            case LEFT:
+                return RIGHT;
+
+            case RIGHT:
+                return LEFT;
+        }
+
         return null;
     }
+
+
+
+
+
+    private Direction changeFromVertical() {
+
+        if (Math.random() < 0.5) {
+            return LEFT;
+        }
+        return RIGHT;
+
+    }
+
+
+    private Direction changeFromHorizontal() {
+
+        if (Math.random() < 0.5) {
+            return UP;
+        }
+        return DOWN;
+    }
+
 }
