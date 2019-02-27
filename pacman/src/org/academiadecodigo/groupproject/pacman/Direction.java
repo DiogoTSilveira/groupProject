6f062d1132e4c94189ce24d22ff2343ca6fa295d
Code Cadet <codecadet@lovelace.local>
2019-02-27 18:55:08 -0100

    fagsdhj

--- pacman/src/org/academiadecodigo/groupproject/pacman/Direction.java
+++ pacman/src/org/academiadecodigo/groupproject/pacman/Direction.java
@@ -5,13 +5,17 @@ package org.academiadecodigo.groupproject.pacman;
  */
 
 public enum Direction {
-    UP,
-    DOWN,
-    LEFT,
-    RIGHT;
+    UP(0,-5),
+    DOWN(0,5),
+    LEFT(-5,0),
+    RIGHT(5,0);
+    private int col;
+    private int row;
 
-
-
+    Direction(int col, int row){
+        this.col = col;
+        this.row = row;
+    }
 
     public static Direction randomDirection(){
 
