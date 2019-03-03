ddbfc5f5c656b9a0e231cbc39c0865778d2ba5d9
Code Cadet <codecadet@lovelace.local>
2019-03-03 16:34:56 -0100

    Player more pretty

--- pacman/src/org/academiadecodigo/groupproject/pacman/gameobjects/Player.java
+++ pacman/src/org/academiadecodigo/groupproject/pacman/gameobjects/Player.java
@@ -26,11 +26,11 @@ public class Player extends GameObject {
         this.collisionDetector = collisionDetector;
         player = (Ellipse) super.shape;
         player.draw();
-        right = new Picture(player.getX(), player.getY(), "resources/Pacman/RIGHT.png");
+        right = new Picture(310, 330, "resources/Pacman/RIGHT.png");
         picture = right;
-        down = new Picture(player.getX(), player.getY(), "resources/Pacman/DOWN.png");
-        up = new Picture(player.getX(), player.getY(), "resources/Pacman/UP.png");
-        left = new Picture(player.getX(), player.getY(), "resources/Pacman/Left.png");
+        down = new Picture(310, 330, "resources/Pacman/DOWN.png");
+        up = new Picture(310, 330, "resources/Pacman/UP.png");
+        left = new Picture(310, 330, "resources/Pacman/Left.png");
 
         picture.draw();
     }
@@ -50,64 +50,23 @@ public class Player extends GameObject {
         this.direction = direction;
         switch (direction) {
             case UP:
-
-                player.setColor(Color.BLUE);
                 this.direction = Direction.UP;
-
-
-                //picture.delete();
-                int initialXUpPosition = up.getX();
-                int initialYUpPosition = up.getY();
-
-                up.translate(player.getX() - initialXUpPosition, player.getY() - initialYUpPosition);
-                picture = up;
-
-                picture.draw();
-
+                newPicture(up);
                 break;
 
             case DOWN:
-
-                player.setColor(Color.YELLOW);
                 this.direction = Direction.DOWN;
-
-                //picture.delete();
-                int initialXDownPosition = down.getX();
-                int initialYDownPosition = down.getY();
-
-                down.translate(player.getX() - initialXDownPosition, player.getY() - initialYDownPosition);
-                picture = down;
-                picture.draw();
-
+                newPicture(down);
                 break;
 
             case RIGHT:
-
-                player.setColor(Color.BLACK);
                 this.direction = Direction.RIGHT;
-
-                //picture.delete();
-                int initialXRightPosition = right.getX();
-                int initialYRightPosition = right.getY();
-                right.translate(player.getX() - initialXRightPosition, player.getY() - initialYRightPosition);
-                picture = right;
-                picture.draw();
-
-
+                newPicture(right);
                 break;
 
             case LEFT:
-
-                player.setColor(Color.CYAN);
                 this.direction = Direction.LEFT;
-
-                //picture.delete();
-                int initialXLeftPosition = left.getX();
-                int initialYLeftPosition = left.getY();
-                left.translate(player.getX() - initialXLeftPosition, player.getY() - initialYLeftPosition);
-                picture = left;
-                picture.draw();
-
+                newPicture(left);
                 break;
 
         }
@@ -142,6 +101,20 @@ public class Player extends GameObject {
 
     public int getRow() {
         return direction.getRow();
+
+    }
+    private void translatePicture(Picture picture){
+        int initialXPosition = picture.getX();
+        int initialYPosition = picture.getY();
+        picture.translate(player.getX() - initialXPosition, player.getY() - initialYPosition);
+
+    }
+    private void newPicture(Picture picture){
+        this.picture.delete();
+        translatePicture(picture);
+        this.picture = picture;
+        this.picture.draw();
+
 
     }
 
