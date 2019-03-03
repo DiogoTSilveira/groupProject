e896a5a6c5d5f0e377c1550a4eb93ce8fea1dd39
Diogo Silveira <diogotostesilveira@gmail.com>
2019-03-03 22:43:19 -0100

    Adding sounds to the game!

--- pacman/src/org/academiadecodigo/groupproject/pacman/Game.java
+++ pacman/src/org/academiadecodigo/groupproject/pacman/Game.java
@@ -10,10 +10,9 @@ import org.academiadecodigo.groupproject.pacman.gameobjects.Player;
 import org.academiadecodigo.groupproject.pacman.gameobjects.WallFactory;
 import org.academiadecodigo.groupproject.pacman.keyboard.KeyboardListener;
 import org.academiadecodigo.simplegraphics.graphics.Color;
-import org.academiadecodigo.simplegraphics.graphics.Ellipse;
 import org.academiadecodigo.simplegraphics.graphics.Rectangle;
-import org.academiadecodigo.simplegraphics.graphics.Shape;
 import org.academiadecodigo.simplegraphics.pictures.Picture;
+import org.academiadecodigo.groupproject.pacman.*;
 
 public class Game {
 
@@ -54,7 +53,7 @@ public class Game {
     }
 
     private void createBackgroundAndField() {
-        Picture background = new Picture(PADDING, PADDING, "resources/Background/oie_0AT68Uz38HJQ.jpg");
+        Picture background = new Picture(PADDING, PADDING, "pacman/resources/Background/oie_0AT68Uz38HJQ.jpg");
         background.draw();
 
         Rectangle field = new Rectangle(6 * cellSize + PADDING, 10 * cellSize + PADDING, 48 * cellSize, 40 * cellSize);
@@ -62,7 +61,7 @@ public class Game {
         field.fill();
     }
 
-    public void initializeCollisionDetector(){
+    public void initializeCollisionDetector() {
         collisionDetector = new CollisionDetector();
         collisionDetector.setWalls(WallFactory.createGameField());
     }
@@ -72,12 +71,12 @@ public class Game {
         keyboardListener = new KeyboardListener(player);
 
 
-        Ghost ghost = new Ghost(264 + 10, collisionDetector, "resources/mcs/chapeu jojo.png");
-        Ghost ghost1 = new Ghost(294, collisionDetector, "resources/mcs/chapeu nuno.png");
-        Ghost ghost2 = new Ghost(314, collisionDetector, "resources/mcs/chapeu ruben.png" );
-        Ghost ghost3 = new Ghost(334, collisionDetector, "resources/mcs/chapeu rudy.png");
-        Ghost ghost4 = new Ghost(354, collisionDetector, "resources/mcs/chapeu seringa.png");
-        Ghost ghost5 = new Ghost(374, collisionDetector, "resources/mcs/chapeu xico.png");
+        Ghost ghost = new Ghost(264 + 10, collisionDetector, "pacman/resources/mcs/chapeu jojo.png");
+        Ghost ghost1 = new Ghost(294, collisionDetector, "pacman/resources/mcs/chapeu nuno.png");
+        Ghost ghost2 = new Ghost(314, collisionDetector, "pacman/resources/mcs/chapeu ruben.png");
+        Ghost ghost3 = new Ghost(334, collisionDetector, "pacman/resources/mcs/chapeu rudy.png");
+        Ghost ghost4 = new Ghost(354, collisionDetector, "pacman/resources/mcs/chapeu seringa.png");
+        Ghost ghost5 = new Ghost(374, collisionDetector, "pacman/resources/mcs/chapeu xico.png");
         this.ghost = new Ghost[]{ghost, ghost1, ghost2, ghost3, ghost4, ghost5};
     }
 
@@ -85,29 +84,37 @@ public class Game {
      * This method will set loop which never ends unless the player colides with a ghost.
      *
      * @throws InterruptedException if any thread has interrupted the current thread.
-     * The interrupted status of the current thread is cleared when this exception is thrown.
-     *
-     * Every time the player movement is null it will proceed to the next iteration of the loop. This happens in the beginning.
-     *
-     * After the player moves, it will check collisions with other ghosts. If true, breaks the loop but if not, the ghosts will
-     * move.
+     *                              The interrupted status of the current thread is cleared when this exception is thrown.
+     *                              <p>
+     *                              Every time the player movement is null it will proceed to the next iteration of the loop. This happens in the beginning.
+     *                              <p>
+     *                              After the player moves, it will check collisions with other ghosts. If true, breaks the loop but if not, the ghosts will
+     *                              move.
      */
 
     public void start() throws InterruptedException {
-
+        Sound wakawaka = new Sound("/pacman/resources/sound/wakawaka.wav");
+        wakawaka.setLoop(-1);
         while (true) {
+
             if (player.getDirection() == null) {
+                wakawaka.stop();
                 continue;
             }
 
             Thread.sleep(20);
             player.move();
+
             if (collisionDetector.checkCollisionWithGhosts(player, ghost)) {
 
-                //This is causing a IIOException: Can't read input file
+                wakawaka.stop();
 
-     //           Picture gameOver = new Picture(20,20, "resources/gameover.png");
-       //         gameOver.draw();
+                Sound over = new Sound("/pacman/resources/sound/summarizersound.wav");
+                over.play(true);
+
+                Picture gameOver = new Picture(200, 200, "pacman/resources/Texts/gameover.png");
+                gameOver.grow(100,100);
+                gameOver.draw();
 
                 break;
             }
@@ -118,7 +125,7 @@ public class Game {
     }
 
     public void moveGhosts() {
-        for (Ghost ghost: ghost) {
+        for (Ghost ghost : ghost) {
             ghost.move();
         }
     }
