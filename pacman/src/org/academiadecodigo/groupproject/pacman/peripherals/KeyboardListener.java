6550c4f4f9786abb655c37ad8b51144b11f0e501
Diogo Silveira <diogotostesilveira@gmail.com>
2019-03-04 19:06:08 -0100

    Added keys SPACE and R

--- pacman/src/org/academiadecodigo/groupproject/pacman/peripherals/KeyboardListener.java
+++ pacman/src/org/academiadecodigo/groupproject/pacman/peripherals/KeyboardListener.java
@@ -1,4 +1,4 @@
-package org.academiadecodigo.groupproject.pacman.keyboard;
+package org.academiadecodigo.groupproject.pacman.peripherals;
 
 import org.academiadecodigo.groupproject.pacman.Direction;
 import org.academiadecodigo.groupproject.pacman.gameobjects.Player;
@@ -15,17 +15,29 @@ public class KeyboardListener implements KeyboardHandler {
 
     private Player player;
 
+    Keyboard keyboard;
+
     //Setting our keys to control the player.
     KeyboardEvent up = new KeyboardEvent();
     KeyboardEvent down = new KeyboardEvent();
     KeyboardEvent right = new KeyboardEvent();
     KeyboardEvent left = new KeyboardEvent();
+    KeyboardEvent space = new KeyboardEvent();
+    KeyboardEvent r = new KeyboardEvent();
 
-    public KeyboardListener(Player player){
+    public KeyboardListener(Player player) {
         this.player = player;
 
-        //Creating a keyboard to be able to move our shape.
-        Keyboard keyboard = new Keyboard(this);
+        //Creating a peripherals to be able to move our shape.
+        keyboard = new Keyboard(this);
+
+        setKeyboardEventType();
+        setKey();
+        addEventListener();
+
+    }
+
+    private void setKeyboardEventType() {
 
         //All keys will be pressed ONCE and the player will move until we press another key. This key will change the current direction.
         up.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
@@ -33,33 +45,71 @@ public class KeyboardListener implements KeyboardHandler {
         right.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
         left.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
 
+        space.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        space.setKeyboardEventType(KeyboardEventType.KEY_RELEASED);
+
+        r.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+
+    }
+
+    private void setKey() {
+
         up.setKey(KeyboardEvent.KEY_UP);
         down.setKey(KeyboardEvent.KEY_DOWN);
         right.setKey(KeyboardEvent.KEY_RIGHT);
         left.setKey(KeyboardEvent.KEY_LEFT);
+
+        space.setKey(KeyboardEvent.KEY_SPACE);
+
+        r.setKey(KeyboardEvent.KEY_R);
+
+
+    }
+
+    private void addEventListener() {
 
         keyboard.addEventListener(up);
         keyboard.addEventListener(down);
         keyboard.addEventListener(right);
         keyboard.addEventListener(left);
 
+        keyboard.addEventListener(space);
+
+        keyboard.addEventListener(r);
+
     }
 
     @Override
     public void keyPressed(KeyboardEvent keyboardEvent) {
 
-        switch (keyboardEvent.getKey()){
+        switch (keyboardEvent.getKey()) {
+
             case KeyboardEvent.KEY_UP:
+
                 player.setDirection(Direction.UP);
                 break;
+
             case KeyboardEvent.KEY_DOWN:
+
                 player.setDirection(Direction.DOWN);
                 break;
+
             case KeyboardEvent.KEY_RIGHT:
+
                 player.setDirection(Direction.RIGHT);
                 break;
+
             case KeyboardEvent.KEY_LEFT:
+
                 player.setDirection(Direction.LEFT);
+                break;
+
+            case KeyboardEvent.KEY_SPACE:
+
+                break;
+
+            case KeyboardEvent.KEY_R:
+
                 break;
         }
 
@@ -69,5 +119,11 @@ public class KeyboardListener implements KeyboardHandler {
     @Override
     public void keyReleased(KeyboardEvent keyboardEvent) {
 
+        switch (keyboardEvent.getKey()) {
+
+            case KeyboardEvent.KEY_SPACE:
+
+                break;
+        }
     }
 }
---
+++ pacman/src/org/academiadecodigo/groupproject/pacman/peripherals/MouseListener.java
@@ -0,0 +1,37 @@
+package org.academiadecodigo.groupproject.pacman.peripherals;
+
+import org.academiadecodigo.groupproject.pacman.gameobjects.*;
+import org.academiadecodigo.simplegraphics.mouse.MouseEvent;
+import org.academiadecodigo.simplegraphics.mouse.MouseEventType;
+import org.academiadecodigo.simplegraphics.mouse.MouseHandler;
+import org.academiadecodigo.simplegraphics.mouse.Mouse;
+
+public class MouseListener implements MouseHandler {
+
+    Player player;
+
+    Mouse mouse;
+
+    public MouseListener(Player player){
+
+        this.player = player;
+
+        mouse = new Mouse(this);
+
+
+    }
+
+
+
+    @Override
+    public void mouseClicked(MouseEvent mouseEvent) {
+
+
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent mouseEvent) {
+
+    }
+}
