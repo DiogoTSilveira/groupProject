8621caae77fe463412fd835086a77dd31d3204da
Code Cadet <codecadet@lovelace.local>
2019-03-04 17:05:41 -0100

    Player now changes direction only when he can. When pressing a key to set direction, the pacman will only turn when he finds a spot to do so.

--- pacman/src/org/academiadecodigo/groupproject/pacman/gameobjects/Player.java
+++ pacman/src/org/academiadecodigo/groupproject/pacman/gameobjects/Player.java
@@ -18,12 +18,14 @@ public class Player extends GameObject {
     private Picture left;
     private Picture right;
     private Ellipse player;
+    private Direction oldDirection;
+    private Picture oldPicture;
+    private Direction nextDirection;
 
     public Player(CollisionDetector collisionDetector) {
         super(new Ellipse(304 + 10, 324 + 10, 12, 12), null);
         this.collisionDetector = collisionDetector;
         player = (Ellipse) super.shape;
-        player.draw();
         right = new Picture(310, 330, "resources/Pacman/RIGHT.png");
         down = new Picture(310, 330, "resources/Pacman/DOWN.png");
         up = new Picture(310, 330, "resources/Pacman/UP.png");
@@ -44,25 +46,23 @@ public class Player extends GameObject {
 
     public void setDirection(Direction direction) {
         this.direction = direction;
+
         switch (direction) {
+
             case UP:
-                this.direction = Direction.UP;
-                newPicture(up);
+                this.nextDirection = Direction.UP;
                 break;
 
             case DOWN:
-                this.direction = Direction.DOWN;
-                newPicture(down);
+                this.nextDirection = Direction.DOWN;
                 break;
 
             case RIGHT:
-                this.direction = Direction.RIGHT;
-                newPicture(right);
+                this.nextDirection = Direction.RIGHT;
                 break;
 
             case LEFT:
-                this.direction = Direction.LEFT;
-                newPicture(left);
+                this.nextDirection = Direction.LEFT;
                 break;
         }
     }
@@ -73,22 +73,30 @@ public class Player extends GameObject {
             picture.translate(0, 0);
             return;
         }
-
+        tryDirection();
         player.translate(direction.getX() * 2, direction.getY() * 2);
         picture.translate(direction.getX() * 2, direction.getY() * 2);
-        //Thread.sleep(1);
 
         if (collisionDetector.checkCollisionWithWalls(this)) {
             player.translate(-direction.getX() * 2, -direction.getY() * 2);
             picture.translate(-direction.getX() * 2, -direction.getY() * 2);
+            if (picture == null || oldPicture == null) {
+                return;
+            }
+            direction = oldDirection;
+            newPicture(oldPicture);
             return;
         }
+        oldPicture = picture;
+        newPicture(getPicture(direction.name()));
+        oldDirection = direction;
     }
 
     private void translatePicture(Picture picture) {
         int initialXPosition = picture.getX();
         int initialYPosition = picture.getY();
         picture.translate(player.getX() - initialXPosition - 4, player.getY() - initialYPosition - 4);
+
     }
 
     private void newPicture(Picture picture) {
@@ -99,5 +107,43 @@ public class Player extends GameObject {
 
 
     }
+
+    private Picture getPicture(String picture) {
+        if (picture == "UP") {
+            return up;
+        }
+        if (picture == "DOWN") {
+            return down;
+        }
+        if (picture == "LEFT") {
+            return left;
+        }
+        return right;
+
+    }
+
+    private boolean tryDirection() {
+        player.translate(nextDirection.getX() * 2, nextDirection.getY() * 2);
+
+        if (collisionDetector.checkCollisionWithWalls(this)) {
+            player.translate(-nextDirection.getX() * 2, -nextDirection.getY() * 2);
+            return false;
+        }
+        player.translate(-nextDirection.getX() * 2, -nextDirection.getY() * 2);
+        if (direction != nextDirection) {
+            player.translate(direction.getX() * 2, direction.getY() * 2);
+        }
+        direction = nextDirection;
+        return true;
+
+    }
+
+    public int dash(Boolean isdash) {
+        if (isdash) {
+            return 2;
+        }
+        return 1;
+    }
+
 
 }
